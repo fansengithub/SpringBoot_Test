4ab99ac1830a83ec3e83de62fa272c8a9dd024e5
fansengithub <sfan@ctrip.com>
2019-01-16 15:45:52 +0800

    启动main()方法修改。

--- src/main/java/com/example/demo/NewGirlApplication.java
+++ src/main/java/com/example/demo/NewGirlApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NewGirlApplication {
 
 	public static void main(String[] args) {
+		System.out.println("springBoot test");
 		SpringApplication.run(NewGirlApplication.class, args);
 	}
 }
