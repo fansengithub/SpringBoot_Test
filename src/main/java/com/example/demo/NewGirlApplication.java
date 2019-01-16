7368528588aa3802c71f4e6ece376afcf7137ce3
fansengithub <sfan@Ctrip.com>
2019-01-16 16:31:54 +0800

    启动main()添加注释。

--- src/main/java/com/example/demo/NewGirlApplication.java
+++ src/main/java/com/example/demo/NewGirlApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NewGirlApplication {
 
 	public static void main(String[] args) {
-		System.out.println(" test");
+		System.out.println(" test!");
+		// springBoot中的启动main()方法
 		SpringApplication.run(NewGirlApplication.class, args);
 	}
 }
