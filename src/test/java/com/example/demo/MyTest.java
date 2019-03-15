b865f30bf272a7e44a6336c987abe6b06d06d357
sfan <sfan@ctrip.com>
2019-03-15 14:05:48 +0800

    new  envirement test

--- src/test/java/com/example/demo/MyTest.java
+++ src/test/java/com/example/demo/MyTest.java
@@ -3,5 +3,6 @@ package com.example.demo;
 public class MyTest {
     public static void main(String[] args) {
         System.out.println("qwe---");
+        System.out.println("this is my Test");
     }
 }
